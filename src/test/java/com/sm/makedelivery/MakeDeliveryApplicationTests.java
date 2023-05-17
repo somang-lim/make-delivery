78ccf6cc6f11158dcc4ac0b6f8401036c1618921
somang-lim <somangjuha@gmail.com>
2023-05-17 13:35:56 +0900

    #1 - 스프링부트 프로젝트 생성 (Gradle)

---
+++ src/test/java/com/sm/makedelivery/MakeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.makedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
