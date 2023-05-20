5b4df3b38f43b45e294b456081cae87900a3fdb1
somang-lim <somangjuha@gmail.com>
2023-05-20 15:05:21 +0900

    #12 - redis 설정
    redis session stroage를 이용해서 session을 관리할 수 있도록 설정한다.

--- src/main/java/com/sm/makedelivery/service/SessionLoginService.java
+++ src/main/java/com/sm/makedelivery/service/SessionLoginService.java
@@ -1,10 +1,21 @@
 package com.sm.makedelivery.service;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+
+/*
+	SessionLoginService
+	HttpSEssion에 직접 의존하지 않습니다.
+	스프링이 SessionLoginService에 HttpSession 의존성 주입(DI)을 해줍니다.
+	LoginService 인터페이스를 이용해 Session 방식의 LoginService를 구현합니다.
+	SessionLoginService는 singleton scope를 가지며 HttpSession은 session scope를 가집니다.
+	이러한 스코프 차이 때문에 Spring이 HttpSession 인스턴스를 동적 프록시로 생성하여 주입합니다.
+	이러한 기법은 Scoped Proxy라고 합니다.
+ */
 
 @Service
 @Transactional(readOnly = true)
@@ -25,3 +36,10 @@ public class SessionLoginService implements LoginService {
 		session.removeAttribute(USER_ID);
 	}
 }
+
+/*
+	레디스의 키값으로 저장되는 타입입니다.
+	spring:session:sessions:expires (string) 해당 세션의 만료키로 사용합니다.
+	spring:session:expirations (set) expire time에 삭제될 세션 정보를 담고 있습니다.
+	spring:session:sessions (hash) session은 map을 저장소로 사용하기 대문에 이곳에 세션 테이터가 있습니다.
+ */
