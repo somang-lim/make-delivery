e184ccfce71fd7f6529edb672d22f5d45dba9a0a
somang-lim <somangjuha@gmail.com>
2023-05-23 13:15:56 +0900

    #18 - Store 관련 로직 작성
    * DTO
    * Mapper
    * Service
    * Controller
    * 필요한 Exception

---
+++ src/main/java/com/sm/makedelivery/controller/StoreController.java
@@ -0,0 +1,54 @@
+package com.sm.makedelivery.controller;
+
+import static com.sm.makedelivery.annotation.LoginCheck.*;
+import static com.sm.makedelivery.utils.ResponseEntityConstants.RESPONSE_OK;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sm.makedelivery.annotation.CurrentUserId;
+import com.sm.makedelivery.annotation.LoginCheck;
+import com.sm.makedelivery.dto.StoreDTO;
+import com.sm.makedelivery.service.StoreService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/stores")
+@RequiredArgsConstructor
+public class StoreController {
+
+	private final StoreService storeService;
+
+	@PostMapping
+	@LoginCheck(userLevel = UserLevel.OWNER)
+	public ResponseEntity<Void> insertStore(StoreDTO store, @CurrentUserId String ownerId) {
+		storeService.insertStore(store, ownerId);
+
+		return RESPONSE_OK;
+	}
+
+	@GetMapping
+	@LoginCheck(userLevel = UserLevel.OWNER)
+	public ResponseEntity<List<StoreDTO>> getMyAllStore(@CurrentUserId String ownerId) {
+		List<StoreDTO> stores = storeService.getMyAllStore(ownerId);
+
+		return ResponseEntity.ok().body(stores);
+	}
+
+	@GetMapping("/{storeId}")
+	@LoginCheck(userLevel = UserLevel.OWNER)
+	public ResponseEntity<StoreDTO> getMyStore(@PathVariable String storeId, @CurrentUserId String ownerId) {
+		storeService.validateMyStore(storeId, ownerId);
+		StoreDTO store = storeService.getMyStore(storeId, ownerId);
+
+		return ResponseEntity.ok().body(store);
+	}
+
+}
---
+++ src/main/java/com/sm/makedelivery/service/StoreService.java
@@ -0,0 +1,67 @@
+package com.sm.makedelivery.service;
+
+import java.util.List;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
+
+import com.sm.makedelivery.dto.StoreDTO;
+import com.sm.makedelivery.exception.StoreNameAlreadyExistsException;
+import com.sm.makedelivery.mapper.StoreMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class StoreService {
+
+	private final StoreMapper storeMapper;
+
+
+	@Transactional
+	public void insertStore(StoreDTO store, String ownerId) {
+		try {
+			StoreDTO newStore = setOwnerId(store, ownerId);
+			storeMapper.insertStore(newStore);
+		} catch (DuplicateKeyException e) {
+			throw new StoreNameAlreadyExistsException("Same Store Name " + store.getName());
+		}
+	}
+
+	private StoreDTO setOwnerId(StoreDTO store, String ownerId) {
+		StoreDTO newStore = StoreDTO.builder()
+			.name(store.getName())
+			.phone(store.getPhone())
+			.address(store.getAddress())
+			.ownerId(ownerId)
+			.introduction(store.getIntroduction())
+			.build();
+
+		return newStore;
+	}
+
+	public List<StoreDTO> getMyAllStore(String ownerId) {
+		return storeMapper.selectStoreList(ownerId);
+	}
+
+	public boolean isMyStore(String storeId, String ownerId) {
+		return storeMapper.isMyStore(storeId, ownerId);
+	}
+
+	public void validateMyStore(String storeId, String ownerId) {
+		boolean isMyStore = isMyStore(storeId, ownerId);
+
+		if (!isMyStore) {
+			throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+		}
+	}
+
+	public StoreDTO getMyStore(String storeId, String ownerId) {
+		return storeMapper.selectStore(storeId, ownerId);
+	}
+
+}
